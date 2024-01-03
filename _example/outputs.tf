f23c9479296bda55fe71cbafc2c57522b79bfdc3
Vaibhav Bhagwat Pendhare <vaibhav.pendhare@clouddrove.com>
2024-01-04 01:05:14 +0530

    Feat: Add automerge github shared workflow (#22)
    * Feat: Add automerge github shared workflow
    
    * feat: added version.tf in example and fix tflint issue
    
    ---------
    
    Co-authored-by: Anmol Nagpal <anmol@clouddrove.com>

--- _example/outputs.tf
+++ _example/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
-  value       = module.labels.*.id
+  value       = module.labels[*].id
   description = "Disambiguated ID."
 }
 
 output "tags" {
-  value       = module.labels.*.tags
+  value       = module.labels[*].tags
   description = "Normalized Tag map."
 }
 
 output "name" {
-  value       = module.labels.*.name
+  value       = module.labels[*].name
   description = "Normalized Tag map."
 }
